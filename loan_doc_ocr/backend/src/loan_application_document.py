d6319eb54f3808946e00e85397d39cb22fdc7749
Anuarg Kulkarni <141646066+AnuragKulkarni27@users.noreply.github.com>
2025-04-25 18:29:52 +0530

    Update loan_application_document.py

--- loan_doc_ocr/backend/src/loan_application_document.py
+++ loan_doc_ocr/backend/src/loan_application_document.py
@@ -1,11 +1,8 @@
 import re
 from parser_generic import MedicalDocParser
-
-
 class PrescriptionParser(MedicalDocParser):
     def __init__(self, text):
         MedicalDocParser.__init__(self, text)
-        
     def parse(self):
         return{
             "name": self.get_field("name"),
@@ -13,7 +10,6 @@ class PrescriptionParser(MedicalDocParser):
             "income_details": self.get_field("income_details"),
             "loan_amount": self.get_field("loan_amount")
         }        
-    
     def get_field(self, field_name):
         pattern_dict = {
             "name": {"pattern": "Name:(.*)Date", "flags": 0},
@@ -21,7 +17,6 @@ class PrescriptionParser(MedicalDocParser):
             "income_details": {"pattern": "Address:[^\n]*(.*)Directions", "flags": re.DOTALL},
             "loan_amount": {"pattern": "Directions:.(.*)Refill", "flags": re.DOTALL},
         }
-
         pattern_object = pattern_dict.get(field_name)
         if pattern_object:
             matches = re.findall(pattern_object["pattern"], self.text, flags=pattern_object["flags"])
